1df68272cddfa8e950714207eaf700ce7516f7af
Margarida <margaridammcm@gmail.com>
2022-07-19 10:56:50 -0300

    Borda  em cada time dinamica

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -3,7 +3,7 @@ import './Time.css'
 const Time = (props) => {
   return (
     <section className="time" style={{ backgroundColor: props.corSecundaria }}>
-      <h3>{props.nome}</h3>
+      <h3 style={{ borderColor: props.corPrimaria }} >{props.nome}</h3>
     </section>
   )
 }
