246d03d756ad97640555a37b9e6968c7c8e90fda
Margarida <margaridammcm@gmail.com>
2022-07-19 10:50:25 -0300

    Cor do time dinamica

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -2,7 +2,7 @@ import './Time.css'
 
 const Time = (props) => {
   return (
-    <section className="time">
+    <section className="time" style={{ backgroundColor: props.corSecundaria }}>
       <h3>{props.nome}</h3>
     </section>
   )
