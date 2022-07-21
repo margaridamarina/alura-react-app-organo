25f82e1882ad1099ffe8bebc39c111fc8d570f78
Margarida <margaridammcm@gmail.com>
2022-07-20 22:05:52 -0300

    Criando componente colaborador

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,15 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+  return(<div className='colaborador'>
+    <div className='cabecalho'>
+      <img src='https://github.com/MargaridaMarina.png' alt='Margarida Marina'/>
+    </div>
+    <div className='rodape'>
+      <h4>Margarida Marina</h4>
+      <h5>Instrutor(a)</h5>
+    </div>
+  </div>)
+}
+
+export default Colaborador
