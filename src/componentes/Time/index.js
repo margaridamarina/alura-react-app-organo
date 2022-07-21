3a3e44744f91c1f82541e1b9ac42524d5addc52d
Margarida <margaridammcm@gmail.com>
2022-07-20 22:29:31 -0300

    Escondendo time vazio

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -3,7 +3,7 @@ import './Time.css'
 
 const Time = (props) => {
   return (
-    <section className="time" style={{ backgroundColor: props.corSecundaria }}>
+    (props.colaboradores.length > 0) ? <section className="time" style={{ backgroundColor: props.corSecundaria }}>
       <h3 style={{ borderColor: props.corPrimaria }} >{props.nome}</h3>
       <div className='colaboradores'>
         {props.colaboradores.map(colaborador => <Colaborador 
@@ -13,6 +13,7 @@ const Time = (props) => {
                                                 />)}
       </div>
     </section>
+    : ''
   )
 }
 
